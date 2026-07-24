0ecbc723a279c73f663e499879e9be480a7ff275
mvxyoffcial-create <mvxyoffcial@gmail.com>
2026-07-24 14:13:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "123456"))
-    API_HASH = os.environ.get("API_HASH", "your_api_hash")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
+    API_ID = 36282056
+    API_HASH = "3a948acece533f362b4c90b2b3c14b60"
+    BOT_TOKEN = "8285873350:AAHVx971B_3r-lJM804MkH288qqjMWHq_CI"
     
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
+    MONGO_URI = "mongodb+srv://filmzi2120_db_user:zero8907@cluster0.zyau0re.mongodb.net/?appName=Cluster0"
     DATABASE_NAME = "videobot_db"
     
     OWNER_ID = int(os.environ.get("OWNER_ID", "123456789"))
