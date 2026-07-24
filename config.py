a28305e442a02eb5dd874c992a6890e8c7972a94
mvxyoffcial-create <mvxyoffcial@gmail.com>
2026-07-24 14:11:28 +0530

    Replace environment variables with hardcoded values

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
     
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "videobot_db")
+    DATABASE_NAME = "videobot_db"
     
     OWNER_ID = int(os.environ.get("OWNER_ID", "123456789"))
-    FORCE_SUB_CHANNELS = os.environ.get("FORCE_SUB_CHANNELS", "spideyoffcail,mvxyoffcail").split(",")
+    FORCE_SUB_CHANNELS = "spideyoffcail,mvxyoffcail"
     
-    PORT = int(os.environ.get("PORT", "8000"))
-    WORKERS = int(os.environ.get("WORKERS", "500"))
+    PORT = "8000"
+    WORKERS = "500"
     
-    DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
+    DOWNLOAD_DIR = "./downloads"
     
     STICKER_ID = "CAACAgIAAxkBAAEQZtFpgEdROhGouBVFD3e0K-YjmVHwsgACtCMAAphLKUjeub7NKlvk2TgE"
     PICS_URL = ["https://api.aniwallpaper.workers.dev/random?type=girl"]
