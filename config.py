58f1f9287d66ce9e46df35734a8a7f276c8bed71
mvxyoffcial-create <mvxyoffcial@gmail.com>
2026-07-24 12:09:18 +0530

    Change BOT_TOKEN in config.py
    Updated BOT_TOKEN for the bot configuration.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = 36282056
 API_HASH = "3a948acece533f362b4c90b2b3c14b60"
-BOT_TOKEN = "8701355562:AAFJxO2AQPfmMTmKMo7V2jyq2drX7kb9JRo"
+BOT_TOKEN = "8285873350:AAHVx971B_3r-lJM804MkH288qqjMWHq_CI"
 MONGO_URL = "mongodb+srv://filmzi2120_db_user:zero8907@cluster0.zyau0re.mongodb.net/?appName=Cluster0"
 
 ADMIN_IDS = [8498741978]
